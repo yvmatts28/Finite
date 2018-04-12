18e03c71c64b3db47c3f2878ffbce8ee68cb341c
yvmatts28 <work.yashvardhan.mathur@gmail.com>
2018-04-12 15:14:29 +0530

    Problem with SQLite Db. Swipe to delete Added and fixed.

--- app/src/main/java/com/technology/olympian/finite/Activity/MainActivity.kt
+++ app/src/main/java/com/technology/olympian/finite/Activity/MainActivity.kt
@@ -2,24 +2,25 @@ package com.technology.olympian.finite.Activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
-import android.view.MenuItem
+import android.view.View
 import com.franmontiel.fullscreendialog.FullScreenDialogFragment
 import com.technology.olympian.finite.Data.ToDoAdapter
 import com.technology.olympian.finite.Data.ToDoDatabaseHandler
 import com.technology.olympian.finite.Data.ToDoItem
+import com.technology.olympian.finite.Model.SwipeToDeleteCallback
 import com.technology.olympian.finite.R
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class MainActivity : AppCompatActivity(),FullScreenDialogFragment.OnDiscardListener {
 
     var dbHandler:ToDoDatabaseHandler? = null
     var adapter:ToDoAdapter? = null
-    var list:ArrayList<ToDoItem>? = null
+    var list:MutableList<ToDoItem>? = null
     var layoutManager:RecyclerView.LayoutManager? = null
 
     override fun onDiscard() {
@@ -30,8 +31,8 @@ class MainActivity : AppCompatActivity(),FullScreenDialogFragment.OnDiscardListe
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        imageView3.visibility = View.INVISIBLE
+        textView4.visibility = View.INVISIBLE
 
 
         var t = ToDoItem()
@@ -42,18 +43,41 @@ class MainActivity : AppCompatActivity(),FullScreenDialogFragment.OnDiscardListe
 
         dbHandler = ToDoDatabaseHandler(this)
 
-        list = ArrayList<ToDoItem>()
+        list = ArrayList()
         list = dbHandler!!.readAllItems()
         list!!.add(t)
+
+        if((list as ArrayList<ToDoItem>).isEmpty()){
+            imageView3.visibility = View.VISIBLE
+            textView4.visibility = View.VISIBLE
+        }
+        else
+        {
+            imageView3.visibility = View.INVISIBLE
+            textView4.visibility = View.INVISIBLE
+        }
+        recyclerView.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.VERTICAL))
         layoutManager = LinearLayoutManager(this)
         adapter = ToDoAdapter(list!!,this)
 
         recyclerView.layoutManager = layoutManager
-        adapter!!.swipeToDeleteDelegate.pending = true
-        recyclerView.adapter = adapter
 
-        val itemTouchHelper = ItemTouchHelper(adapter?.swipeToDeleteDelegate?.itemTouchCallBack)
+        recyclerView.adapter = adapter
+        val swipeHandler = object : SwipeToDeleteCallback(this) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val adapter = recyclerView.adapter as ToDoAdapter
+                adapter.removeAt(viewHolder.adapterPosition)
+                if((list as ArrayList<ToDoItem>).isEmpty()){
+
+                    imageView3.visibility = View.VISIBLE
+                    textView4.visibility = View.VISIBLE
+                }
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeHandler)
         itemTouchHelper.attachToRecyclerView(recyclerView)
+
+
 
         fab_add.setOnClickListener {
 
--- app/src/main/java/com/technology/olympian/finite/Data/ToDoAdapter.kt
+++ app/src/main/java/com/technology/olympian/finite/Data/ToDoAdapter.kt
@@ -1,31 +1,17 @@
 package com.technology.olympian.finite.Data
 
-import android.animation.ValueAnimator
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.agilie.swipe2delete.ModelOptions
-import com.agilie.swipe2delete.SwipeToDeleteDelegate
-import com.agilie.swipe2delete.interfaces.IAnimationUpdateListener
-import com.agilie.swipe2delete.interfaces.IAnimatorListener
-import com.agilie.swipe2delete.interfaces.ISwipeToDeleteAdapter
-import com.agilie.swipe2delete.interfaces.ISwipeToDeleteHolder
 import com.technology.olympian.finite.R
-import io.realm.RealmBasedRecyclerViewAdapter
-import io.realm.RealmResults
-import io.realm.RealmViewHolder
 
 /**
  * Created by Yash on 25-03-2018.
  */
-class ToDoAdapter(private val list:ArrayList<ToDoItem>,private val context: Context):RecyclerView.Adapter<ToDoAdapter.ViewHolder>(),ISwipeToDeleteAdapter<Int,ToDoItem,ToDoAdapter.ViewHolder>, IAnimationUpdateListener, IAnimatorListener {
-
-    val swipeToDeleteDelegate = SwipeToDeleteDelegate(items = list, swipeToDeleteAdapter = this)
-    var animationEnabled = true
-    var bottomContainer = true
+class ToDoAdapter(private val list:MutableList<ToDoItem>,private val context: Context):RecyclerView.Adapter<ToDoAdapter.ViewHolder>(){
 
 
 
@@ -44,40 +30,22 @@ class ToDoAdapter(private val list:ArrayList<ToDoItem>,private val context: Cont
     }
 
 
-
-    override fun findItemPositionByKey(key: Int) = (0..list.lastIndex).firstOrNull{list[it].getId() == key}?: -1
-
-    override fun onBindCommonItem(holder: ViewHolder, key: Int, item: ToDoItem, position: Int) {
-        holder.apply {
-            var tName = itemView.findViewById<TextView>(R.id.displayName)
-            var tDate = itemView.findViewById<TextView>(R.id.dateDisplay)
-            var tAssignedBy = itemView.findViewById<TextView>(R.id.displayAssignedBy)
-
-            tName.text = item.getName().toString()
-            tDate.text = item.getDate().toString()
-            tAssignedBy.text = item.getAssignedBy().toString()
-        }
+    fun addItem(t:ToDoItem){
+        list.add(t)
+        notifyItemInserted(list.size)
     }
 
-    override fun removeItem(key: Int) {
-        swipeToDeleteDelegate.removeItem(key)
-    }
+    fun removeAt(position: Int){
 
+        list.removeAt(position)
+        notifyItemRemoved(position)
 
-    override fun onAnimationUpdated(animation: ValueAnimator?, options: ModelOptions<*>) {
-       TODO()
     }
 
 
 
-    inner class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView),ISwipeToDeleteHolder<Int> {
+    inner class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
 
-        override var key: Int = -1
-
-        override var pendingDelete: Boolean = false
-
-        override val topContainer: View
-            get() = TODO()
 
         var tName = itemView.findViewById<TextView>(R.id.displayName)
         var tDate = itemView.findViewById<TextView>(R.id.dateDisplay)
